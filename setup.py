3e7a174ac3e1f4091614c7d48f5728728692be82
brsg <@gmail.com>
2016-04-07 14:56:14 -0400

    pypi files update

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'ezlogging',
+  packages = ['ezlogging'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Python wrapper for logging library',
+  author = 'Bobby Grado',
+  author_email = 'bobby.grado@gmail.com',
+  url = 'https://github.com/brsgr/ez-logging', # use the URL to the github repo
+  download_url = 'https://github.com/brsgr/ez-logging/tarball/0.1', # I'll explain this in a second
+  keywords = ['testing', 'logging'], # arbitrary keywords
+  classifiers = []
+)
