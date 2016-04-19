ef4a4478199a2499e1fbe5cc9313b4a0e0709ae8
brsg <@gmail.com>
2016-04-19 11:38:39 -0400

    fixed setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
   author = 'Bobby Grado',
   author_email = 'bobby.grado@gmail.com',
   url = 'https://github.com/brsgr/ez-logging', # use the URL to the github repo
-  download_url = 'https://github.com/brsgr/ez-logging/tarball/0.1', # I'll explain this in a second
+  download_url = 'https://github.com/brsgr/ez-logging/tarball/0.2',
   keywords = ['testing', 'logging'], # arbitrary keywords
   classifiers = []
 )
