0bd08bac838b5cb642448582f034595a27e85c23
brsg <@gmail.com>
2016-04-19 11:13:01 -0400

    updated version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'ezlogging',
   packages = ['ezlogging'], # this must be the same as the name above
-  version = '0.1',
+  version = '0.2',
   description = 'Python wrapper for logging library',
   author = 'Bobby Grado',
   author_email = 'bobby.grado@gmail.com',
